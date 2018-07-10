d89e8b2a175080acfa4c581e42091f7418606ee4
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-10 10:37:30 -0600

    Complete question model

--- questions/models.js
+++ questions/models.js
@@ -2,7 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
-  
+  silhouette : {
+    type: String,
+  },
+  filledIn: {
+    type: String,
+  },
+  answer: {
+    type: String,
+  },
+  correct: {
+    type: Number,
+  },
+  total : {
+    type: Number,
+  },
+  m : {
+    type: Number,
+  }
 });
 
 questionSchema.set('toObject', {
@@ -10,7 +27,6 @@ questionSchema.set('toObject', {
     ret.id = ret._id;
     delete ret._id;
     delete ret.__v;
-    delete ret.password;
   }
 });
 
