f6d15a4a2870bd93f696bd95e04efa7bd3ad5226
Terrance Corley <terrancecorley@outlook.com>
2018-07-10 08:38:53 -0700

    added linked list class

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,35 @@
-//linked list class & node class
-//methods: insertAt() & changeHead() // removeFirst()
 
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertAt(item, position) {
+    let currNode = this.head;
+    let counter = 0;
+    while (currNode.next !== null) {
+      if (counter === position - 1) {
+        currNode.next = new _Node(item, currNode.next);
+        return;
+      } else {
+        counter++;
+        currNode = currNode.next;
+      }
+    }
+    currNode.next = new _Node(item, null);
+    return;
+  }
+
+  removeFirst() {
+    this.head = this.head.next;
+  }
+}
+
+module.exports = {_Node, LinkedList};
