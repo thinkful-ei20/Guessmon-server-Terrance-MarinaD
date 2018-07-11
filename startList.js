b063404a698345ed6fcb480d46a6e13868f6eb52
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-11 12:36:11 -0600

    Add other pokemon

--- startList.js
+++ startList.js
@@ -12,8 +12,8 @@ const pikachu = {
 const head = new _Node(pikachu, null);
 
 const squirtle = {
-  'silhouette': 'http://oi44.tinypic.com/34hbf51.jpg',
-  'filledIn': 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+  'silhouette': 'https://image.ibb.co/cLLXxT/squirtle_sil.png',
+  'filledIn': 'https://image.ibb.co/dii7Wo/squirtle.png',
   'answer': 'squirtle',
   'correct': 0,
   'total': 0,
@@ -22,9 +22,81 @@ const squirtle = {
 
 const bulbasaur = 
 { 
-  'silhouette': 'https://freestencilgallery.com/wp-content/uploads/2017/04/Pokemon-Bulbasaur-Silhouette-Stencil-thumb.jpg',
-  'filledIn': 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+  'silhouette': 'https://image.ibb.co/hkruBo/bulbasaur_sil.png',
+  'filledIn': 'https://image.ibb.co/i3Y7Wo/bulbasaur.png',
   'answer': 'bulbasaur',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const chansey = { 
+  'silhouette': 'https://image.ibb.co/jzB5HT/chansey_sil.png',
+  'filledIn': 'https://image.ibb.co/gkWi48/chansey.png',
+  'answer': 'chansey',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const charmander = {
+  'silhouette': 'https://image.ibb.co/cvZQHT/charmander_sil.png',
+  'filledIn': 'https://image.ibb.co/gf7ecT/charmander.png',
+  'answer': 'charmander',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const exeggutor = {
+  'silhouette': 'https://image.ibb.co/etrSWo/exeggutor_sil.png',
+  'filledIn': 'https://image.ibb.co/ePVbP8/exeggutor.png',
+  'answer': 'exeggutor',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const rattata = {
+  'silhouette': 'https://image.ibb.co/h0aAj8/rattata_sil.png',
+  'filledIn': 'https://image.ibb.co/mOhCxT/rattata.png',
+  'answer': 'rattata',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const sandslash = {
+  'silhouette': 'https://image.ibb.co/jD4QHT/sandslash_sil.png',
+  'filledIn': 'https://image.ibb.co/mWBSWo/sandslash.png',
+  'answer': 'sandslash',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const scyther = {
+  'silhouette': 'https://image.ibb.co/m3i7Wo/scyther_sil.png',
+  'filledIn': 'https://image.ibb.co/kQvbP8/scyther.png',
+  'answer': 'scyther',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const voltorb = {
+  'silhouette': 'https://image.ibb.co/emzQHT/voltorb_sil.png',
+  'filledIn': 'https://image.ibb.co/gDN1ro/voltorb.png',
+  'answer': 'voltorb',
+  'correct': 0,
+  'total': 0,
+  'm': 1
+};
+
+const weepinbell = {
+  'silhouette': 'https://image.ibb.co/nuQzcT/weepinbell_sil.png',
+  'filledIn': 'https://image.ibb.co/m0dwP8/weepinbell.png',
+  'answer': 'weepinbell',
   'correct': 0,
   'total': 0,
   'm': 1
@@ -34,5 +106,19 @@ const defaultList = new LinkedList();
 defaultList.head = head;
 defaultList.insertAt(squirtle, 1);
 defaultList.insertAt(bulbasaur, 2);
+defaultList.insertAt(chansey, 3);
+defaultList.insertAt(charmander, 4);
+defaultList.insertAt(exeggutor, 5);
+defaultList.insertAt(rattata, 6);
+defaultList.insertAt(sandslash, 7);
+defaultList.insertAt(scyther, 8);
+defaultList.insertAt(voltorb, 2);
+defaultList.insertAt(weepinbell, 9);
+
+
+
+
+
+
 
 module.exports = defaultList;
