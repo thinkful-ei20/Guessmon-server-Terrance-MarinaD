726c00fdf7a72d0e7f974bbf3f1f8e06cfbeb340
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-12 09:39:51 -0600

    Update tests to run with remote testing database

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ module.exports = {
   DATABASE_URL:
         process.env.DATABASE_URL,
   TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test',
+        process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY
 //   DATABASE_URL:
--- test/setup.test.js
+++ test/setup.test.js
@@ -1,5 +1,4 @@
-'use strict';
-
+require('dotenv').config();
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
